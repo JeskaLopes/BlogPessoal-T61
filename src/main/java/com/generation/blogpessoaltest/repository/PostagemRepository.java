9e96655f90188e3deb2f0490ee6afe17317893a0
JeskaLopes <jessica97cantora@hotmail.com>
2023-03-15 22:33:15 -0300

    first crud blog pessoal

---
+++ src/main/java/com/generation/blogpessoaltest/repository/PostagemRepository.java
@@ -0,0 +1,16 @@
+package com.generation.blogpessoaltest.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.generation.blogpessoaltest.model.Postagem;
+
+@Repository
+public interface PostagemRepository extends JpaRepository<Postagem, Long>{
+	List<Postagem> findAllByTituloContainingIgnoreCase(@Param("titulo") String titulo );
+				// SELECT * FROM tb_postagens WHERE titulo LIKE "%Spring%"
+	
+}
